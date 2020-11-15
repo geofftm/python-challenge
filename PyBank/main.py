d1370bc10376cf855fc0eb8c11efe04993680c3e
Geoff Matis <gmatis@gmail.com>
2020-11-15 15:57:58 -0500

    new changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,18 +5,52 @@ import csv
 
 csvpath = '/Users/geoffreymatis/Desktop/DS_Bootcamp/Git/python-challenge/PyBank/Resources/budget_data.csv'
 
+months = 0
+value = 0
+totalProfitsLosses = 0
+currentProfitLoss = 0
+lastProfitLoss = 0
+
+monthList = []
+changeList = []
+profitsLosses = []
+valueList = []
+
 with open(csvpath, 'r') as csvfile:
      csvreader = csv.reader(csvfile, delimiter=',')
      csv_header = next(csvreader)
-     monthList = []
-     profitsLosses = []
-     totalProfitsLosses = 0
-     for columns in csvreader:
-        months = columns[0]
-        profits_losses = columns[1]
-        totalProfitsLosses += int(columns[1])
+     
+    #  profitsLosses = []
+    # valueList = []
+    #  changeList = []
+    #  totalProfitsLosses = 0
+     for row in csvreader:
+        month = row[0]
+        profits_losses = row[1]
+        totalProfitsLosses += int(profits_losses)
+        change = int(profits_losses) - value
+        #print(change)
+        value = int(row[1])
         monthList.append(months)
         profitsLosses.append(profits_losses)
+        valueList.append(value)
+        changeList.append(int(change))
+    
+    
      print("Total Months: " + str(len(monthList)))
      print("Total: $" + str(totalProfitsLosses))
+     
+     negativeSum = sum(i for i in changeList if i < 0)
+
+     positiveSum = sum(j for j in changeList if j > 0)
+
+     print(positiveSum)
+     print(negativeSum)
     
+
+     
+  
+
+    
+     
+
