02a5fea3ff12d2fbf1e7a1b0e3d67e3473c0cb65
Geoff Matis <gmatis@gmail.com>
2020-11-20 14:13:43 -0500

    sorted

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,13 +5,20 @@ csvpath = os.path.join('Resources', 'election_data.csv')
 
 candidates = []
 
+totalVotes = 0
+
 with open(csvpath, 'r') as csvfile:
      csvreader = csv.reader(csvfile, delimiter=',')
      csv_header = next(csvreader)
 
      for row in csvreader:
          candidates.append(row[2])
-     print(len(candidates))
+         totalVotes += 1
+     print(totalVotes)
+     print(sorted(candidates))
+
+     
+     
      
 
 
