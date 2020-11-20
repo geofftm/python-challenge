130acddc64df9c0a2afeb9b3742226e5e66fa191
Geoff Matis <gmatis@gmail.com>
2020-11-20 17:04:20 -0500

    periodic change -- comments trying out more dynamic way

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,7 +18,14 @@ with open(csvpath, 'r') as csvfile:
          totalVotes += 1
      
      sortedCandidates = sorted(candidates)
+     
      countedCandidates = Counter (sortedCandidates)
+     
+     voteCount.append(countedCandidates.most_common())
+     
+     #less dynamic way. go into counter object and grab values from keys and run the math that way.
+     #trying a more dynamic way.
+     
      kahnTotal = countedCandidates['Khan']
      correyTotal = countedCandidates['Correy']
      liTotal = countedCandidates['Li']
@@ -28,9 +35,6 @@ with open(csvpath, 'r') as csvfile:
      correyAvg = (correyTotal / totalVotes) * 100
      liAvg = (liTotal / totalVotes) * 100
      oAvg = (oTotal / totalVotes) * 100
-
-     countedList = countedCandidates.most_common()
-     countedListSort = sorted(countedList)
 
      
      
@@ -45,14 +49,15 @@ with open(csvpath, 'r') as csvfile:
     #     print(ele)
         #countedList.append(counted[ele])
 
-     for i, j in countedListSort:
-         voteCount.append(i)
-         voteCount.append(j)
+    #  for i, j in countedListSort:
+    #      voteCount.append(i)
+    #      voteCount.append(j)
      
  
          
 
      print(sortedCandidates)
+     print(voteCount)
      print("Election Results")
      print("-------------------------")
      print(f"Total Votes : {totalVotes}")
@@ -62,8 +67,10 @@ with open(csvpath, 'r') as csvfile:
      print(round(correyAvg, 2))
      print(round(liAvg, 2))
      print(round(oAvg, 2))
-     print(countedList)
-     print(countedListSort)
+
+
+    #  print(countedList)
+    #  print(countedListSort)
 
      
      
