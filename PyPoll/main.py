f3443ee0ce6e3643782a8128905e8213de4624ed
Geoff Matis <gmatis@gmail.com>
2020-11-20 13:51:51 -0500

    changes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,12 +3,15 @@ import csv
 
 csvpath = os.path.join('Resources', 'election_data.csv')
 
+candidates = []
+
 with open(csvpath, 'r') as csvfile:
      csvreader = csv.reader(csvfile, delimiter=',')
      csv_header = next(csvreader)
 
      for row in csvreader:
-         print(row[1])
+         candidates.append(row[1])
+         print(len(candidates))
      
 
 
