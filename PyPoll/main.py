5199fd78d894216c82e21b712866f1c8cfe20b92
Geoff Matis <gmatis@gmail.com>
2020-11-20 14:40:43 -0500

    averages

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -17,12 +17,26 @@ with open(csvpath, 'r') as csvfile:
          totalVotes += 1
      
      counted = Counter(candidates)
+     kahnTotal = counted['Khan']
+     correyTotal = counted['Correy']
+     liTotal = counted['Li']
+     oTotal = counted["O'Tooley"]
+    
+     kahnAvg = round( kahnTotal / totalVotes, 2) * 100
+     correyAvg = round(correyTotal / totalVotes, 2) * 100
+     liAvg = round(liTotal / totalVotes, 2) * 100
+     oAvg = round(oTotal / totalVotes, 2) * 100
+
      print("Election Results")
      print("-------------------------")
      print(f"Total Votes : {totalVotes}")
      print(f"Khan: {counted['Khan']}")
      print(counted)
-     
+     print(oTotal)
+     print(kahnAvg)
+     print(correyAvg)
+     print(liAvg)
+     print(oAvg)
 
      
      
